401b44d6c13c4467cfee5250216aef83dc2c6cc7
Shehara <t2wasu00@students.oamk.fi>
2023-03-30 12:50:17 +0300

    about page and bmi cal added

---
+++ client/TypeScript/bmi.ts
@@ -0,0 +1,13 @@
+class bmi{
+    // gender: string;
+    // age: string;
+    height: number;
+    weight: number;
+    bmi: number;
+
+    constructor(height: number, weight: number) {
+        this.height = height;
+        this.weight = weight;
+    }
+}
+export { bmi };
