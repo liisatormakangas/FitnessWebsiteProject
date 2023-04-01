741005e2c0639eed0f4eca749f56eb995b5be883
Shehara <t2wasu00@students.oamk.fi>
2023-04-01 17:31:49 +0300

    bmi cal and about page updated, ts added

--- client/JavaScript/bmi.js
+++ client/JavaScript/bmi.js
@@ -14,43 +14,46 @@
         }, false);
     });
 })();
-//   create class and functions to calculate bmi and bmi category
-class bmi {
-    constructor(gender, age, height, weight) {
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.bmi = this.calculateBMI();
-        this.category = this.bmiCategory();
-    }
-    calculateBMI() {
-        let bmi = this.weight / Math.pow(this.height / 100, 2);
-        return bmi;
-    }
-    bmiCategory() {
-        let bmi = this.calculateBMI();
-        let category;
-        if (bmi < 18.5) {
-            category = "Underweight";
-        }
-        else if (bmi >= 18.5 && bmi <= 24.9) {
-            category = "Normal weight";
-        }
-        else if (bmi >= 25 && bmi <= 29.9) {
-            category = "Overweight";
-        }
-        else if (bmi >= 30) {
-            category = "Obesity";
-        }
-        return category;
-    }
-}
-// create object
-let gender = document.querySelector('#gender').value;
-let age = document.querySelector('#age').value;
-let height = document.querySelector('#height').value;
-let weight = document.querySelector('#weight').value;
-let result = document.querySelector('#bmi').value;
-let category = document.querySelector('#category').value;
-let bmiObj = new bmi(gender, age, height, weight);
+// //   create class and functions to calculate bmi and bmi category
+//   class bmi{
+//     gender: number;
+//     age: number;
+//     height: number;
+//     weight: number;
+//     bmi: number;
+//     category: string;
+//     constructor(gender:number, age: number, height: number, weight: number) {
+//         this.gender = gender;
+//         this.age = age;
+//         this.height = height;
+//         this.weight = weight;
+//         this.bmi = this.calculateBMI();
+//         this.category = this.bmiCategory();
+//     }
+//     calculateBMI(): number {
+//         let bmi = this.weight / Math.pow(this.height/100, 2);
+//         return bmi;
+//     }
+//     bmiCategory(): string {
+//         let bmi = this.calculateBMI();
+//         let category: string;
+//         if (bmi < 18.5) {
+//             category = "Underweight";
+//         } else if (bmi >= 18.5 && bmi <= 24.9) {
+//             category = "Normal weight";
+//         } else if (bmi >= 25 && bmi <= 29.9) {
+//             category = "Overweight";
+//         } else if (bmi >= 30) {
+//             category = "Obesity";
+//         }
+//         return category;
+//     }
+// }
+// // create object
+// let gender = (document.querySelector('#gender') as HTMLInputElement).value;
+// let age = (document.querySelector('#age') as HTMLInputElement).value;
+// let height = (document.querySelector('#height')as HTMLInputElement).value;
+// let weight = (document.querySelector('#weight')as HTMLInputElement).value;
+// let result = (document.querySelector('#bmi')as HTMLInputElement).value;
+// let category = (document.querySelector('#category')as HTMLInputElement).value;
+// let bmiObj = new bmi(gender,age,height,weight);
--- client/TypeScript/bmi.ts
+++ client/TypeScript/bmi.ts
@@ -18,49 +18,49 @@
       });
   })();
 
-//   create class and functions to calculate bmi and bmi category
-  class bmi{
-    gender: number;
-    age: number;
-    height: number;
-    weight: number;
-    bmi: number;
-    category: string;
+// //   create class and functions to calculate bmi and bmi category
+//   class bmi{
+//     gender: number;
+//     age: number;
+//     height: number;
+//     weight: number;
+//     bmi: number;
+//     category: string;
 
-    constructor(gender:number, age: number, height: number, weight: number) {
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.bmi = this.calculateBMI();
-        this.category = this.bmiCategory();
-    }
-    calculateBMI(): number {
-        let bmi = this.weight / Math.pow(this.height/100, 2);
-        return bmi;
-    }
-    bmiCategory(): string {
-        let bmi = this.calculateBMI();
-        let category: string;
-        if (bmi < 18.5) {
-            category = "Underweight";
-        } else if (bmi >= 18.5 && bmi <= 24.9) {
-            category = "Normal weight";
-        } else if (bmi >= 25 && bmi <= 29.9) {
-            category = "Overweight";
-        } else if (bmi >= 30) {
-            category = "Obesity";
-        }
-        return category;
-    }
+//     constructor(gender:number, age: number, height: number, weight: number) {
+//         this.gender = gender;
+//         this.age = age;
+//         this.height = height;
+//         this.weight = weight;
+//         this.bmi = this.calculateBMI();
+//         this.category = this.bmiCategory();
+//     }
+//     calculateBMI(): number {
+//         let bmi = this.weight / Math.pow(this.height/100, 2);
+//         return bmi;
+//     }
+//     bmiCategory(): string {
+//         let bmi = this.calculateBMI();
+//         let category: string;
+//         if (bmi < 18.5) {
+//             category = "Underweight";
+//         } else if (bmi >= 18.5 && bmi <= 24.9) {
+//             category = "Normal weight";
+//         } else if (bmi >= 25 && bmi <= 29.9) {
+//             category = "Overweight";
+//         } else if (bmi >= 30) {
+//             category = "Obesity";
+//         }
+//         return category;
+//     }
    
-}
-// create object
-let gender = (document.querySelector('#gender') as HTMLInputElement).value;
-let age = (document.querySelector('#age') as HTMLInputElement).value;
-let height = (document.querySelector('#height')as HTMLInputElement).value;
-let weight = (document.querySelector('#weight')as HTMLInputElement).value;
-let result = (document.querySelector('#bmi')as HTMLInputElement).value;
-let category = (document.querySelector('#category')as HTMLInputElement).value;
+// }
+// // create object
+// let gender = (document.querySelector('#gender') as HTMLInputElement).value;
+// let age = (document.querySelector('#age') as HTMLInputElement).value;
+// let height = (document.querySelector('#height')as HTMLInputElement).value;
+// let weight = (document.querySelector('#weight')as HTMLInputElement).value;
+// let result = (document.querySelector('#bmi')as HTMLInputElement).value;
+// let category = (document.querySelector('#category')as HTMLInputElement).value;
 
-let bmiObj = new bmi(gender,age,height,weight);
+// let bmiObj = new bmi(gender,age,height,weight);
