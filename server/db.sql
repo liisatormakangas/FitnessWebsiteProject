da1ec509d43fc45376f4f1ac2133d69426d62e4f
Huyen Nguyen <c2ngth00@students.oamk.fi>
2023-04-13 11:30:40 +0300

    added more info for the course tableThanh92

--- server/db.sql
+++ server/db.sql
@@ -17,7 +17,11 @@ create table users (
 
 create table courses (
     id_course serial primary key,
-    course_image varchar(255),
+    name_image1 varchar(100),
+    extra_image2 varchar(100),
+    extra_image3 varchar(100),
+    extra_image4 varchar(100),
+    video_name varchar(100),
     course_name varchar(50),
     trainer_name varchar(50),
     course_description varchar(5000),
@@ -25,7 +29,7 @@ create table courses (
     weekends varchar(50),
     weekday_duration varchar(50),
     weekend_duration varchar(50),
-    place varchar(50),
+    place varchar(100),
     available_seats int,
     price_month float,    
     price_year float
@@ -122,7 +126,11 @@ insert into stories (
 update stories set story=' ' where id_story = 1;
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -136,7 +144,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Yoga1.jpg',
+        'Yoga1.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Yoga.mp4',
         'Body Balance',
         'John Doe',
         'This body balance yoga course is designed to improve stability and coordination through a series of postures and movements. The classes focus on building strength in the core, legs, and feet, while also incorporating mindfulness techniques to enhance body awareness and control. Students of all levels can benefit from this course, which offers a supportive and energizing environment for developing greater balance and harmony within the body.',
@@ -151,7 +163,11 @@ insert into courses (
         );
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -165,7 +181,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Body Step.jpg',
+        'Body Step.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Body Step.mp4',
         'Body Step',
         'Emily Parker',
         'Body step exercise is a cardio-based workout that uses basic stepping movements to improve fitness and stamina. It involves a series of choreographed routines set to music, which makes the workout fun and engaging.',
@@ -180,7 +200,11 @@ insert into courses (
         );
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -194,7 +218,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Body attack.jpg',
+        'Body attack.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Body attack.mp4',
         'Body Attack',
         'Sophia Lee',
         'Body attack is a high-energy, sports-inspired group fitness class that combines aerobic and plyometric movements with strength and stabilization exercises. Designed to improve cardiovascular fitness, agility, and overall physical endurance, Body attack provides a challenging workout suitable for individuals of all fitness levels.',
@@ -210,7 +238,11 @@ insert into courses (
 
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -224,7 +256,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Aerobic.jpg',
+        'Aerobic.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Aerobic.mp4',
         'Aerobic',
         'Samuel Kim',
         'Aerobic exercise classes involve continuous and rhythmic movements that increase the heart rate and improve cardiovascular endurance. These classes can be high-impact or low-impact, and may include a variety of exercises such as dance, step aerobics, kickboxing, and cycling, making them a fun and effective way to burn calories and improve overall fitness.',
@@ -239,7 +275,11 @@ insert into courses (
         );
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -253,7 +293,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Core.jpg',
+        'Core.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Core.mp4',
         'Core',
         'Chloe Davis',
         'Core exercise classes focus on strengthening and toning the muscles in the abdomen, back, and pelvis. These classes often include exercises such as planks, crunches, and bridges, and can be tailored to challenge individuals of all fitness levels, helping to improve posture, stability, and overall core strength.',
@@ -268,7 +312,11 @@ insert into courses (
         );
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -282,7 +330,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Body building.jpg',
+        'Body building.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Body building.mp4',
         'Body Building',
         'Olivia Ramirez',
         'Bodybuilding exercise classes are designed to help individuals build muscle and increase strength through weightlifting and resistance training. These classes often focus on specific muscle groups and may include exercises such as squats, deadlifts, and bench presses, making them an effective way to sculpt and tone the body while improving overall fitness.',
@@ -297,7 +349,11 @@ insert into courses (
         );
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -311,7 +367,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Crossfit.jpg',
+        'Crossfit.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Crossfit.mp4',
         'Crossfit',
         'OBenjamin Ortiz',
         'CrossFit exercise classes combine high-intensity interval training, weightlifting, and gymnastics to create a challenging and diverse workout that targets all aspects of fitness. These classes often involve a combination of functional movements performed at a high intensity, making them a great way to improve strength, endurance, and overall fitness.',
@@ -326,7 +386,11 @@ insert into courses (
         );
 
 insert into courses (
-    course_image,
+    name_image1,
+    extra_image2,
+    extra_image3,
+    extra_image4,
+    video_name,
     course_name,
     trainer_name,
     course_description,
@@ -340,7 +404,11 @@ insert into courses (
     price_year
     )
     values (
-        './images/Barre.jpg',
+        'Barre.jpg',
+        'Yoga2.jpg',
+        'Yoga3.jpg',
+        'Yoga4.jpg',
+        'Barre.mp4',
         'Barre',
         'Brandon Collins',
         'Barre exercise classes are inspired by ballet and incorporate elements of dance, Pilates, and yoga to create a low-impact, full-body workout. These classes focus on small, isometric movements that target the muscles in the legs, core, and arms, while also improving flexibility, balance, and posture, making them a great choice for individuals of all fitness levels.',
