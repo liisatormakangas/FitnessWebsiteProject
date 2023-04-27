e9985374c6679782e3ec3e96ec06d4cba55baee6
Cheng Shufen <ophelia.scarlett007@gmail.com>
2023-04-27 09:40:16 +0300

    stash

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const pool = new pg_1.Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'FitnessWebSite',
-    password: process.env.DATABASE_PASSWORD,
+
     //change to get password from .env file
     password: process.env.DATABASE_PASSWORD,
     port: 5432,
